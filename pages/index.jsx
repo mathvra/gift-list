99a69aeeb064d5a3a31d6ef47cc070b5b5c32198
mathvra <matheusaraujo@meto.do>
2022-08-20 21:55:44 -0300

    maps church

--- pages/index.jsx
+++ pages/index.jsx
@@ -43,6 +43,8 @@ export default function Home() {
         height={1000}
         width={2000}
         alt="EmillyEMax" />
+
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.0888128323318!2d-35.20947508405657!3d-5.843106159216033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ff791ac68411%3A0x9eaba592f340dca3!2sIgreja%20de%20Santo%20Afonso!5e0!3m2!1spt-BR!2sbr!4v1661043239913!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:'0'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <h1>
         Lista de presentes
       </h1>
