8d3e9f3e08f71eb48a83a966c7d30aa18c9b6fe4
mathvra <matheusvram@gmail.com>
2022-08-24 22:08:14 -0300

    settings for the desktop

--- pages/index.jsx
+++ pages/index.jsx
@@ -31,12 +31,14 @@ export default function Home() {
     <div>
       <title>Emilly e Max ♥</title>
       <Header />
-      <Image
-        className={styles.bannerMain}
-        src="/emillymax2.webp"
-        height={2000}
-        width={3000}
-        alt="Emilly e Max" />
+      <div className={styles.containerBannerMain}>
+        <Image
+          className={styles.bannerMain}
+          src="/emillymax2.webp"
+          height={2000}
+          width={3000}
+          alt="Emilly e Max" />
+      </div>
       <div className={styles.bottomBanner}></div>
       <div className={styles.container}>
         <div className={styles.mapsChurch}>
@@ -49,13 +51,13 @@ export default function Home() {
           <h1>Cerimônia</h1>
           <Image
             className={styles.ImgChurch}
-            src="/santo_afonso.jpg"
-            height={1536}
-            width={2048}
+            src="/santo_afonso.png"
+            height={608}
+            width={1080}
             alt="Santo Afonso" />
-          <p>Igreja Santo Afonso</p>
-          <p>Gostaríamos muito de contar com a presença de todos vocês no momento em que nossa união será abençoada diante de Deus! Contamos com vocês!</p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.0888128323318!2d-35.20947508405657!3d-5.843106159216033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ff791ac68411%3A0x9eaba592f340dca3!2sIgreja%20de%20Santo%20Afonso!5e0!3m2!1spt-BR!2sbr!4v1661043239913!5m2!1spt-BR!2sbr" style={{ border: '0', width: '100%', height: '250px' }} loading="lazy"></iframe>
+          <p className={styles.legendImage}>Igreja Santo Afonso</p>
+          <p className={styles.textCall}>Gostaríamos muito de contar com a presença de todos vocês no momento em que nossa união será abençoada diante de Deus! Contamos com vocês!</p>
+          <iframe className={styles.iframeChurch} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.0888128323318!2d-35.20947508405657!3d-5.843106159216033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ff791ac68411%3A0x9eaba592f340dca3!2sIgreja%20de%20Santo%20Afonso!5e0!3m2!1spt-BR!2sbr!4v1661043239913!5m2!1spt-BR!2sbr" style={{ border: '0', width: '100%', height: '250px' }} loading="lazy"></iframe>
         </div>
 
         <div className={styles.mapsBuffet}>
@@ -72,9 +74,9 @@ export default function Home() {
             height={608}
             width={1080}
             alt="Santo Afonso" />
-          <p>Solar Imperial</p>
-          <p>Os noivos convidam para recepção no Solar Imperial após cerimônia religiosa.</p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.2308549559193!2d-35.21705708405683!3d-5.823035459039341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b255601fdc0ec9%3A0xc530dde69af66eba!2sSolar%20Imperial%20%7C%20Recep%C3%A7%C3%B5es%20%7C%20Natal!5e0!3m2!1spt-BR!2sbr!4v1661219886014!5m2!1spt-BR!2sbr" style={{ border: '0', width: '100%', height: '250px' }} loading="lazy"></iframe>
+          <p className={styles.legendImage}>Solar Imperial</p>
+          <p className={styles.textCall}>Os noivos convidam para recepção no Solar Imperial após cerimônia religiosa.</p>
+          <iframe className={styles.iframeBuffet} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.2308549559193!2d-35.21705708405683!3d-5.823035459039341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b255601fdc0ec9%3A0xc530dde69af66eba!2sSolar%20Imperial%20%7C%20Recep%C3%A7%C3%B5es%20%7C%20Natal!5e0!3m2!1spt-BR!2sbr!4v1661219886014!5m2!1spt-BR!2sbr" style={{ border: '0', width: '100%', height: '250px' }} loading="lazy"></iframe>
         </div>
 
         <div className={styles.listGifts}>
@@ -103,7 +105,7 @@ export default function Home() {
                 Endereço para entrega:
               </b><br />
               Rua das Verbenas, 65, Capim Macio, Natal/RN <br />
-              CEP: 59032-550
+              CEP: 59078-090
             </p>
           </div>
           <div className={styles.signedList}>
